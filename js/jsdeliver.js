49f6b725a3ea9201a87b48444d9e9c931cbfa6c7
Alva <1090034020@qq.com>
2020-10-24 16:40:49 +0800

    Site updated: 2020-10-24 16:40:40

--- js/jsdeliver.js
+++ js/jsdeliver.js
@@ -1,6 +1,6 @@
 // jsdeliver CDN 加速图片加载
 var name = "A1va";
-var repository = "blog";
+var repository = "A1va.github.io";
 if(document.domain != 'localhost'){
 	if(document.getElementById("post")){
 		for (var key in document.getElementById("article-container").getElementsByTagName('img')){
